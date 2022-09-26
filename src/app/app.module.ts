6be50e5867dfa685392c44407cf0bbf7b20e22d9
Miguel Trubiano <trubiano59@hotmail.com>
2022-09-26 19:01:54 -0300

    cambios mal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+
 
 
 
@@ -11,7 +11,7 @@ import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+ 
    
   ],
   imports: [
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+
 import { LogoApComponent } from './logo-ap/logo-ap.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    LogoApComponent,
+   
+    LogoApComponent
   ],
   
   imports: [
