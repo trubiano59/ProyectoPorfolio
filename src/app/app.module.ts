967e2e290ebfff398b6f058ff86366c27e746c7e
Miguel Trubiano <trubiano59@hotmail.com>
2022-09-27 01:14:17 -0300

    arreglos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
 
 
 
@@ -11,14 +11,14 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
- 
+    HeaderComponent
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    
+
  ],
   providers: [],
   bootstrap: [AppComponent]
